92377b7ae113098528f6a319f17d83b438438f16
GUILHERME DE OLIVEIRA RODRIGUES <guilherme.orodrigue3@senacsp.edu.br>
2022-08-29 14:43:44 -0300

    Adicionar arquivos de projeto.

---
+++ Ex.01 - Hello Word/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Ex._01___Hello_Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Agora estou Programando");
+            Console.WriteLine("Guilherme de Oliveira Rodrigues");
+            Console.WriteLine("Vai Corinthians");
+        }
+    }
+}
