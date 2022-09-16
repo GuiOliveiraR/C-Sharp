07aac6b3224950c6770830a3081f7282bc65b9b1
GUILHERME DE OLIVEIRA RODRIGUES <guilherme.orodrigue3@senacsp.edu.br>
2022-09-16 15:02:58 -0300

    Curso CSharp

--- Ex.13/Program.cs
+++ Ex.13/Program.cs
@@ -6,7 +6,17 @@ namespace Ex._13
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Insira o horario: ");
+            double h = double.Parse(Console.ReadLine());
+
+            if (h >= 7 && h < 12)
+            {
+                Console.WriteLine("BOM DIA!");
+            }
+            if (h >= 13 && h < 18)
+            {
+                Console.WriteLine("BOM TARDE!");
+            }
         }
     }
 }
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira o horario: ");
+            double h = double.Parse(Console.ReadLine());
+
+            if (h >= 1 && h < 6)
+            {
+                Console.WriteLine("Boa Madrugada!");
+            }
+            else if (h >= 6 && h < 12)
+            {
+                Console.WriteLine("BOM DIA!");
+            }
+            else if (h >= 12 && h < 18)
+            {
+                Console.WriteLine("BOM TARDE!");
+            }
+            else if (h >= 18 && h <= 24)
+            {
+                Console.WriteLine("BOM NOITE!");
+            }
+            else
+            { 
+                Console.WriteLine("Hora Inválida");
+            }
+        }
+    }
+}
